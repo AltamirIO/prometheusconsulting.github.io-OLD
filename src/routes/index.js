6cfd94c2ff239bb1f6f0e65e35b7dce74fdc690d
Michel Paupulaire <mpaupulaire@rms.com>
2018-06-06 19:32:37 -0600

    added netlify

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
 import Services from './Services'
 import Contact from './Contact'
+import Admin from './Admin'
 
 class PrometheusRoutes extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class PrometheusRoutes extends React.Component {
         <div>
           <Route path="/Services" component={Services} />
           <Route path="/Contact" component={Contact} />
+          <Route path="/Admin" component={Admin} />
           <Route exact path="/" component={Home} />
         </div>
       </Router>
