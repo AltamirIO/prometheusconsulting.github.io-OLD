11fa7e4f565a3ba6867e9f563e6df8ed01e20586
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-06-11 18:28:56 -0600

    added contat modal

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,20 +1,31 @@
-import React from "react"
-import { DefaultLayout } from "../../layouts"
-import {
-  Hero,
-  Section,
-  Modal
-} from 'prometheusui'
-import CodeHero from "../../assets/images/code-hero.png"
+import React from 'react'
+import { DefaultLayout } from '../../layouts'
+import { Hero, Section } from 'prometheusui'
+import { Modal } from 'bloomer'
+import CodeHero from '../../assets/images/code-hero.png'
 import LogoGallery from '../../assets/images/logos.png'
 import AppWireframe from '../../assets/images/appwireframe.jpg'
-import "./Home.css"
+import ContactForm from './components/contactForm'
+import './Home.css'
+import { ModalBackground } from 'bloomer/lib/components/Modal/ModalBackground'
+import { ModalContent } from 'bloomer/lib/components/Modal/ModalContent'
+import { ModalClose } from 'bloomer/lib/components/Modal/ModalClose'
+import { ModalCard } from 'bloomer/lib/components/Modal/Card/ModalCard';
+import { ModalCardHeader } from 'bloomer/lib/components/Modal/Card/ModalCardHeader';
+import { ModalCardTitle } from 'bloomer/lib/components/Modal/Card/ModalCardTitle';
+import { Delete } from 'bloomer/lib/elements/Delete';
+import { ModalCardBody } from 'bloomer/lib/components/Modal/Card/ModalCardBody';
+import { ModalCardFooter } from 'bloomer/lib/components/Modal/Card/ModalCardFooter';
+import { Button } from 'bloomer/lib/elements/Button';
+import { Control } from 'bloomer/lib/elements/Form/Control';
+import { Input } from 'bloomer/lib/elements/Form/Input';
+import { Label } from 'bloomer/lib/elements/Form/Label';
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isModalOpen: false
+      isModalOpen: false,
     }
   }
   openModal = () => {
@@ -25,19 +36,15 @@ class HomePage extends React.Component {
   }
   render() {
     const qualityCodeHero = (
-      <Hero.Image
-        image={CodeHero}
-        isSuccess
-        isBold
-        isLarge
-        isFullheight
-      >
+      <Hero.Image image={CodeHero} isSuccess isBold isLarge isFullheight>
         <Hero.Cover />
         <Hero.Body>
           <Hero.Title>Quality Code. On Time, Every Time.</Hero.Title>
           <Hero.Subtitle>
             <p>
-              Prometheus Software Consulting can take care the hassle out of large tech projects,<br/>so your business can get back to doing what it does best.
+              Prometheus Software Consulting can take care the hassle out of
+              large tech projects,<br />so your business can get back to doing
+              what it does best.
             </p>
           </Hero.Subtitle>
           <Hero.CallToAction isRounded onClick={this.openModal}>
@@ -66,7 +73,20 @@ class HomePage extends React.Component {
     )
     const contactModal = (
       <Modal onClose={this.closeModal} isActive={this.state.isModalOpen}>
-        <div style={{backgroundColor: 'white', height: '500px', width: '500px' }}>Hello!</div>
+        <ModalBackground />
+        <ModalCard>
+          <ModalCardHeader>
+            <ModalCardTitle>Contact Us</ModalCardTitle>
+            <Delete onClick={this.closeModal} />
+          </ModalCardHeader>
+          <ModalCardBody>
+            <ContactForm />
+          </ModalCardBody>
+          <ModalCardFooter>
+            <Button isColor="primary">Send</Button>
+            <Button isColor="outline">Cancel</Button>
+          </ModalCardFooter>
+        </ModalCard>
       </Modal>
     )
     return (
